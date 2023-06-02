59c27d525443e13ae397f8e0e747571b605299cf
Daye Kim <dykim315@naver.com>
2023-06-03 01:06:41 +0900

    Initial Commit

---
+++ Practice iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice iOS
+//
+//  Created by 김다예 on 2023/06/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
