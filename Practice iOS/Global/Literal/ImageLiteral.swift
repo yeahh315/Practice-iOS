1b75e7253e60d25ce4941c40a62ba47e0c905295
Daye Kim <dykim315@naver.com>
2023-06-06 16:07:47 +0900

    [ADD] Assets 추가

---
+++ Practice iOS/Global/Literal/ImageLiteral.swift
@@ -0,0 +1,16 @@
+//
+//  ImageLiteral.swift
+//  Practice iOS
+//
+//  Created by 김다예 on 2023/06/06.
+//
+
+import UIKit
+
+struct ImageLiterals {
+    
+    struct Icon {
+        static let back = UIImage(named: "backImg.png")
+        static let next = UIImage(named: "nextImg.png")
+    }
+}
